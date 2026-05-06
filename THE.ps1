00c03842764570166c40175d1f51c9ef888a96d6
Tadaaa06 <tmkwende0@gmail.com>
2026-05-06 23:18:29 +0200

    Add files via upload

---
+++ THE.ps1
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit: Graduation Parking Areas Map"
+git remote add origin https://github.com/YOUR_USERNAME/graduation-parking-areas-map.git
+git branch -M main
+git push -u origin main
